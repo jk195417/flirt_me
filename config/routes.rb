66a767cb7dcbbe06fc6ca1ff19cda7f90f76b969
楊竑昕 <jk195417@gmail.com>
2017-11-17 21:39:55 +0800

    root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  # 首頁
+  root to: 'pages#index'
+  resources :pages, only: [] do
+    collection do
+      get 'index'
+    end
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
