d58cb877765adac40a8c690b1d62ecdba66bdad9
楊竑昕 <jk195417@gmail.com>
2017-11-18 22:04:42 +0800

    navbar logo sign in sign up

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def nav_li(text, path, klass: '', method: :get, inner_class: '', target: nil)
+    active = request.path == path ? 'active' : ''
+    klass += ' nav-item'
+    content_tag :li, link_to(text, path, method: method, class: "nav-link #{active} #{inner_class}", target: target), class: klass
+  end
 end
