43a5cfd7cef7e36905f65c4b86d03f018c32e74d
楊竑昕 <jk195417@gmail.com>
2017-11-27 18:18:00 +0800

    db create table dialogues

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171124052625) do
+ActiveRecord::Schema.define(version: 20171127051208) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "dialogues", force: :cascade do |t|
+    t.string "title"
+    t.string "source"
+    t.integer "likes"
+    t.integer "dislikes"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_dialogues_on_user_id"
+  end
 
   create_table "roles", force: :cascade do |t|
     t.string "name"
