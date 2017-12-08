20be0b12b27a35621a50f5302c776de2e7b2569d
楊竑昕 <jk195417@gmail.com>
2017-12-08 15:37:37 +0800

    resources dialogues

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   namespace :backstage do
     root to: 'pages#index'
     get 'pages/index'
+    resources :dialogues
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
