9a0f13fe471bc33111e3a45967159ca22fe97752
楊竑昕 <jk195417@gmail.com>
2017-12-09 12:30:04 +0800

    sidebar card

--- app/helpers/backstage/sidebar_helper.rb
+++ app/helpers/backstage/sidebar_helper.rb
@@ -3,4 +3,12 @@ module Backstage::SidebarHelper
     icon = content_tag :i, nil, 'data-feather' => 'sidebar'
     content_tag :div, icon, id: 'sidebar-toggler', class: 'btn btn-outline-dark'
   end
+
+  def sidebar_card_link(content, path, klass: '', id: nil)
+    border = 'rounded-0 border-bottom-0 border-right-0 border-left-0'
+    # active = (request.path == path ? 'active' : '')
+    # all_class = "#{border} #{klass} #{active}"
+    all_class = "#{border} #{klass}"
+    link_to content, path, class: all_class, id: id
+  end
 end
