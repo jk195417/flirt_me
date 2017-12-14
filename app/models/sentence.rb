5330e0b3797e23a0cffa2135907643d29be43401
楊竑昕 <jk195417@gmail.com>
2017-12-14 21:20:31 +0800

    update sentence validation

--- app/models/sentence.rb
+++ app/models/sentence.rb
@@ -3,5 +3,9 @@ class Sentence < ApplicationRecord
     MAX = 5
   end
   belongs_to :dialogue
-  validates_presence_of :type, :content, :sequence
+  validates_presence_of :type, :content
+  validates_numericality_of :sequence,
+                            only_integer: true,
+                            greater_than_or_equal_to: 1,
+                            less_than_or_equal_to: Sequence::MAX
 end
