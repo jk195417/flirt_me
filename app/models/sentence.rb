9c3137bb6f0d4d160dea8fbfea031d91656e0858
楊竑昕 <jk195417@gmail.com>
2017-12-14 21:21:02 +0800

    add sentence scope #to_flirt_me_struct

--- app/models/sentence.rb
+++ app/models/sentence.rb
@@ -8,4 +8,16 @@ class Sentence < ApplicationRecord
                             only_integer: true,
                             greater_than_or_equal_to: 1,
                             less_than_or_equal_to: Sequence::MAX
+
+  scope :to_flirt_me_struct, -> {
+    result = {
+      q: Array.new(Sequence::MAX) { Array.new },
+      a: Array.new(Sequence::MAX) { Array.new }
+    }
+    order(sequence: :asc).each do |s|
+      result[:q][s.sequence-1] << s if s.is_a? Question
+      result[:a][s.sequence-1] << s if s.is_a? Answer
+    end
+    result
+  }
 end
