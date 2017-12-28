e7e85f641a12624aeb5bc53731a49093e17d1efc
楊竑昕 <jk195417@gmail.com>
2017-12-28 23:40:19 +0800

    try set host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,5 +90,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # set host
-  config.action_mailer.default_url_options = { host: ENV['HOST'] }
+  config.action_mailer.default_url_options = { host: 'flirt-me.herokuapp.com' }
+  # config.action_mailer.default_url_options = { host: ENV['HOST'] }
 end
