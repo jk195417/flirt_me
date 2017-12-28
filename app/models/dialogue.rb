9365e6b59cdc4cf2d757d93b1f3a19e46199ed79
楊竑昕 <jk195417@gmail.com>
2017-12-28 09:09:59 +0800

    show #id only if seq == 1

--- app/models/dialogue.rb
+++ app/models/dialogue.rb
@@ -16,11 +16,12 @@ class Dialogue < ApplicationRecord
     d_id ||= ramdom_id
     q = Question.where(dialogue_id: d_id, sequence: sequence)
     a = Answer.where(dialogue_id: d_id, sequence: sequence)
+    text = (sequence == 1 ? "##{d_id}\n\n#{q.content}" : q.content)
     if a.blank?
       LineBot::Formats::Messages::Text.new(q.content)
     else
-      t = LineBot::Formats::Templates::Buttons.new("##{d_id}\n\n#{q.content}", a.to_line_actions)
-      LineBot::Formats::Messages::Template.new("撩妹金句##{d_id}", t)
+      template = LineBot::Formats::Templates::Buttons.new(text, a.to_line_actions)
+      LineBot::Formats::Messages::Template.new("撩妹金句##{d_id}", template)
     end
   end
 end
